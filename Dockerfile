aec59c397db1b7afee51783de48dc15bff01efa5
Daniel Flook <daniel@flook.org>
2024-09-30 18:51:42 +0100

    Test commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM alpine
+
+RUN apk add --no-cache git jq
+
+RUN git config --global init.defaultBranch main \
+ && git config --global gc.auto 0 \
+ && git config --global checkout.workers -1 \
+ && git config --global core.fsync none \
+ && git config --global fetch.parallel 0
+
+COPY entrypoint.sh /entrypoint.sh
+#ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+printenv
+
+# If the inputs are not set, error
+if [ -z "$INPUT_REPOSITORY" ]; then
+  echo "Set the repository input"
+  exit 1
+fi
+
+# If the inputs are not set, error
+if [ -z "$INPUT_PATH" ]; then
+  echo "Set the path input"
+  exit 1
+fi
+
+# If the inputs are not set, error
+if [ -z "$INPUT_TOKEN" ]; then
+  echo "Set the token input"
+  exit 1
+fi
+
+REF=${INPUT_REF:-$GITHUB_REF}
+
+rm -rf "${GITHUB_WORKSPACE:?}/$INPUT_PATH"
+git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" init "$GITHUB_WORKSPACE/$INPUT_PATH"
+git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" remote add origin https://github.com/$INPUT_REPOSITORY
+git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" fetch --depth=1 origin $REF
+git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" checkout --force $REF
+
+echo "::set-output name=ref::$REF"
+
+COMMIT=$(git log -1 --format='%H')
+echo "::set-output name=commit::$COMMIT"
