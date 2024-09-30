1860948a91a3836e1850bc0409c1b5e5deb22708
Daniel Flook <daniel@flook.org>
2024-09-30 19:01:59 +0100

    Test commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,7 @@ fi
 REF=${INPUT_REF:-$GITHUB_REF}
 
 #rm -rf "${GITHUB_WORKSPACE:?}/$INPUT_PATH"
+git config --global --add safe.directory "${GITHUB_WORKSPACE:?}/$INPUT_PATH"
 git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" init "$GITHUB_WORKSPACE/$INPUT_PATH"
 git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" remote add origin https://github.com/$INPUT_REPOSITORY
 git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" fetch --depth=1 origin $REF
