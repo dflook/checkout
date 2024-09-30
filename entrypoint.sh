2bcdd874b610b0c656ce41a791e7bea96039785b
Daniel Flook <daniel@flook.org>
2024-09-30 21:35:37 +0100

    Test commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,17 +22,70 @@ if [ -z "$INPUT_TOKEN" ]; then
   exit 1
 fi
 
+GIT_PATH="${GITHUB_WORKSPACE:?}/$INPUT_PATH"
+
+remote_branch_exists() {
+  branch=$1
+  if [ "$(git branch --list --remote origin/$branch)" = "" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+##
+# The arguments to pass to `git checkout` to checkout the ref
+#
+# The argument is the ref to checkout, and may be a branch, tag, or commit.
+# This typically comes from $GITHUB_REF, but can be specified as an input.
+#
+# Outputs a string of arguments to pass to `git checkout`.
+#
+# @param $1 The ref to checkout.
+#
+checkout_args() {
+  ref=$1
+
+  if [ "$ref" = "refs/heads/*" ]; then
+    # remote branch
+    branch=$(echo "$ref" | sed 's|refs/heads/||')
+    echo "-B $branch refs/remotes/origin/$branch"
+  elif [ "$ref" = "refs/pull/*" ]; then
+    # pull request
+    pr_number=$(echo "$ref" | sed 's|refs/pull/||')
+    echo "refs/remotes/pull/$pr_number"
+  elif [ "$ref" = "refs/tags/*" ]; then
+    # qualified ref
+    echo "$ref"
+  elif [ "$ref" = "refs/*" ]; then
+    echo "$ref"
+  else
+    # unqualified ref
+
+    if remote_branch_exists "$ref"; then
+      # remote branch
+      echo "-B $ref refs/remotes/origin/$ref"
+    else
+      # must be a tag
+      echo "refs/tags/$ref"
+    fi
+  fi
+
+}
+
 REF=${INPUT_REF:-$GITHUB_REF}
 
-#rm -rf "${GITHUB_WORKSPACE:?}/$INPUT_PATH"
+#rm -rf $GIT_PATH
 git config --global --add safe.directory "${GITHUB_WORKSPACE:?}"
 git config --list --show-origin
 
-mkdir -p "${GITHUB_WORKSPACE:?}/$INPUT_PATH"
-git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" init "$GITHUB_WORKSPACE/$INPUT_PATH"
-git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" remote add origin https://github.com/$INPUT_REPOSITORY
-git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" fetch --depth=1 origin $REF
-git -C "${GITHUB_WORKSPACE:?}/$INPUT_PATH" checkout --force $REF
+mkdir -p $GIT_PATH
+git -C $GIT_PATH init "$GITHUB_WORKSPACE/$INPUT_PATH"
+git -C $GIT_PATH remote add origin https://github.com/$INPUT_REPOSITORY
+git -C $GIT_PATH fetch --depth=1 origin $REF
+
+# shellcheck disable=SC2046
+git -C $GIT_PATH checkout --force $(checkout_args $REF)
 
 echo "::set-output name=ref::$REF"
 
